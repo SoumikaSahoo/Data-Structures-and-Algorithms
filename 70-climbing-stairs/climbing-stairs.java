ca5d50163814d0537ded75c5f36fa82a24f46fc2
Soumika Sahoo <sahoosoumika247@gmail.com>
2026-07-19 11:54:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (36.18%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,21 +1,12 @@
 class Solution {
-    HashMap<Integer, Integer> dp = new HashMap<>();
-
-    int fun(int i, int n) {
-        if (i == n) return 1;
-        if (i > n) return 0;
-
-        if (dp.containsKey(i))
-            return dp.get(i);
-
-        int ans = fun(i + 1, n) + fun(i + 2, n);
-
-        dp.put(i, ans);
-
-        return ans;
-    }
-
-    public int climbStairs(int n) {
-        return fun(0, n);
-    }
+public int climbStairs(int n) {
+if(n<=2)return n;
+int[]dp=new int[n+1];
+dp[0]=1;
+dp[1]=1;
+for(int i=2;i<=n;i++){
+    dp[i]=dp[i-1]+dp[i-2];
+}
+return dp[n];
+}
 }
